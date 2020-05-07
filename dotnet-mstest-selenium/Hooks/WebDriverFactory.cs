2130a2c050faab91f57e44c8d2b519bb0f602d69
Andrey Suprano <andsuprano@gmail.com>
2020-05-07 09:30:14 -0300

    Add class WebDriverFactory

---
+++ dotnet-mstest-selenium/Hooks/WebDriverFactory.cs
@@ -0,0 +1,108 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Security.Policy;
+using TechTalk.SpecFlow;
+
+namespace dotnet_mstest_selenium.Hooks
+{
+    public class WebDriverFactory
+    {
+        private String navegador, tipoDeExecucao;
+        private IWebDriver _driver;
+        private ScenarioContext scenarioContext;
+
+        public WebDriverFactory( string navegador, string tipoDeExecucao, ScenarioContext scenarioContext)
+        {
+            this.navegador = navegador;
+            this.tipoDeExecucao = tipoDeExecucao;
+            this.scenarioContext = scenarioContext;
+        }
+
+       
+        public WebDriverFactory InstanciaWebDriver()
+        {            
+
+            if(tipoDeExecucao == "local")
+            {
+                InstanciaDriverLocal();
+            }
+            else if(tipoDeExecucao == "remoto")
+            {
+                InstaciaRemoteDriver();
+            }
+            return this;
+        }
+
+        public IWebDriver InstanciaDriverLocal()
+        {
+            var outPutDirectory =
+            Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            switch (navegador)
+            {
+                case "Chrome":
+                    _driver = new ChromeDriver(outPutDirectory);
+                    break;
+                case "Firefox":
+                    _driver = new FirefoxDriver(outPutDirectory);
+                    break;
+            }
+            return _driver;
+        }
+
+
+        private IWebDriver InstaciaRemoteDriver()
+        {
+            var hubUri = new Uri("http://localhost:2222/wd/hub");
+
+            switch(navegador)
+            {
+                case "Chrome":                
+                    var chromeOptions = new ChromeOptions();
+                    chromeOptions.AddArgument("headless");
+                    chromeOptions.AddArgument("disable-gpu");
+                    chromeOptions.PlatformName = "WINDOWS";
+                    chromeOptions.AddArgument("--start-maximized");
+
+                    _driver = new RemoteWebDriver(hubUri, chromeOptions);
+                    break;
+
+
+                case "Firefox":
+                    var firefoxOptions = new FirefoxOptions();
+                   //firefoxOptions.AddArgument("headless");
+                    firefoxOptions.AddArgument("disable-gpu");
+                    firefoxOptions.PlatformName = "WINDOWS";
+                    firefoxOptions.AddArgument("--start-maximized");
+
+                    _driver = new RemoteWebDriver(hubUri, firefoxOptions);
+                    break;
+            }
+            
+            return _driver;
+
+        }
+
+        public WebDriverFactory InserirNoContextoDoTeste()
+        {
+            scenarioContext["DRIVER"] = _driver;            
+            return this;
+        }
+
+        public IWebDriver GetDriver()
+        {
+            InstanciaWebDriver().
+                InserirNoContextoDoTeste();
+            return _driver;
+        }
+
+       
+    }
+}
